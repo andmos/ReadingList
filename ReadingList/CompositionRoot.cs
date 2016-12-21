39316a68164e7e4b2a8b8cb58910ba0848221efd
Andreas Mosti <andreas.mosti@gmail.com>
2016-12-21 10:46:59 +0100

    initial commit, packages

---
+++ ReadingList/CompositionRoot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using LightInject; 
+namespace ReadingList
+{
+	public class CompositionRoot : ICompositionRoot
+	{
+
+		public void Compose(IServiceRegistry serviceRegistry)
+		{
+			
+		}
+	}
+}
---
+++ ReadingList/Trello/TrelloAuthorizationWrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Manatee.Trello;
+using Manatee.Trello.ManateeJson;
+using Manatee.Trello.WebApi;
+
+namespace ReadingList
+{
+	public class TrelloAuthorizationWrapper : ITrelloAuthorizationWrapper
+	{
+		private string ApiKey { get; set; }
+		private string UserToken { get; set; }
+
+		public TrelloAuthorizationWrapper(string apiKey, string userToken)
+		{
+			if (string.IsNullOrEmpty(apiKey) || string.IsNullOrEmpty(userToken)) 
+			{
+				throw new ArgumentNullException(); 
+			}
+		}
+
+		public void Authorize() 
+		{
+			var serializer = new ManateeSerializer();
+			TrelloConfiguration.Serializer = serializer;
+			TrelloConfiguration.Deserializer = serializer;
+			TrelloConfiguration.JsonFactory = new ManateeFactory();
+			TrelloConfiguration.RestClientProvider = new WebApiClientProvider();
+			TrelloAuthorization.Default.AppKey = ApiKey;
+			TrelloAuthorization.Default.UserToken = UserToken;
+		}
+	
+	}
+}
