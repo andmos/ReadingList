7d1e4590194c54985c1d446357ed7f510333e75f
Andreas Mosti <andreas.mosti@gmail.com>
2017-01-11 18:28:12 +0100

    now with working services

---
+++ ReadingList/Modules/ReadingListModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Nancy;
+
+namespace ReadingList
+{
+	public class ReadingListModule : NancyModule 
+	{
+		private readonly IReadingListService m_readingListService;
+		private readonly ITrelloAuthorizationWrapper m_trelloAuth; 
+
+		public ReadingListModule(ITrelloAuthorizationWrapper trelloAuthService, IReadingListService readingListService) : base("/api/")
+		{
+			if (m_trelloAuth == null) 
+			{
+				m_trelloAuth = trelloAuthService;
+			}
+
+			m_readingListService = readingListService;
+
+			Get["/readingList"] = parameters =>
+			{
+				var readingList = m_readingListService.GetReadingList(TrelloBoardConstans.CurrentlyReading);
+				return Response.AsJson(readingList); 
+			};
+
+			Get["/backlogList"] = parameters =>
+			{
+				var readingList = m_readingListService.GetReadingList(TrelloBoardConstans.Backlog);
+				return Response.AsJson(readingList);
+			};
+
+			Get["/doneList"] = parameters =>
+			{
+				var readingList = m_readingListService.GetReadingList(TrelloBoardConstans.DoneReading);
+				return Response.AsJson(readingList);
+			};
+		
+		}
+	
+	}
+}
---
+++ ReadingList/ReadinListSelfHost.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace ReadingList
+{
+	public class ReadinListSelfHost
+	{
+
+		public bool Start()
+		{
+			return true;
+		}
+
+		public bool Stop()
+		{
+			return false;
+		}	
+	}
+}
