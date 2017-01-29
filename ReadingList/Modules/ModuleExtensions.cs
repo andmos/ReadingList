0924b377221b2ec7c37f3174a9bb9418138bd266
Andreas Mosti <andreas.mosti@gmail.com>
2017-01-29 10:57:25 +0100

    added CORS extension for #1

---
+++ ReadingList/Modules/ModuleExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Nancy;
+
+namespace ReadingList
+{
+	public static class ModuleExtensions
+	{
+		public static void EnableCors(this NancyModule module)
+		{
+			module.After.AddItemToEndOfPipeline(x =>
+			{
+				x.Response.WithHeader("Access-Control-Allow-Origin", "*");
+			});
+		}
+	}
+}
