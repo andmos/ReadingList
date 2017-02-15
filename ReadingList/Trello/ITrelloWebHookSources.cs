0a390db0fe2da811481ed4df63922d82369fb15f
Andreas Mosti <andreas.mosti@gmail.com>
2017-02-15 18:47:30 +0100

    started work on webhooks and validation

---
+++ ReadingList/Trello/ITrelloWebHookSources.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ReadingList
+{
+	public interface ITrelloWebHookSources
+	{
+		IEnumerable<string> ValidWebhookSources();
+	}
+}
---
+++ ReadingList/Trello/TrelloWebHookSourcesConfigFileReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+
+namespace ReadingList
+{
+	public class TrelloWebHookSourcesConfigFileReader : ITrelloWebHookSources
+	{
+
+		private readonly char SourcesDelimitorCharacter = ',';
+
+		public IEnumerable<string> ValidWebhookSources()
+		{
+			return ConfigurationManager.AppSettings.Get("WebhookSources").Split(SourcesDelimitorCharacter);
+		}
+
+
+	}
+}
