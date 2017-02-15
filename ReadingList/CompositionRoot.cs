bfa6614721059beb899987d7ffb17dff850cca19
Andreas Mosti <andreas.mosti@gmail.com>
2017-02-15 18:47:30 +0100

    started work on webhooks and validation

--- ReadingList/CompositionRoot.cs
+++ ReadingList/CompositionRoot.cs
@@ -8,6 +8,7 @@ namespace ReadingList
 		public void Compose(IServiceRegistry serviceRegistry)
 		{
 			serviceRegistry.Register<ITrelloAuthorizationWrapper>(factory => new TrelloAuthorizationWrapper(ConfigurationManager.AppSettings["TrelloAPIKey"], ConfigurationManager.AppSettings["TrelloUserToken"]), new PerContainerLifetime());
+			serviceRegistry.Register<ITrelloWebHookSources, TrelloWebHookSourcesConfigFileReader>();
 			serviceRegistry.Register<IBookParser, TrelloBookParser>();
 			serviceRegistry.Register<IReadingListService>(factory => new ReadingListService(TrelloBoardConstans.BoardId, TrelloBoardConstans.ReadingListId, factory.GetInstance<IBookParser>()),new PerContainerLifetime());
 		}
