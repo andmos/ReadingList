2a9b24a5efcf8ad7cffaae72c5c5f578d896037f
andmos <andreas.mosti@gmail.com>
2017-02-16 22:26:25 +0100

    implementet webhook caller

---
+++ ReadingList/Trello/IWebHookCaller.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ReadingList
+{
+	public interface IWebHookCaller
+	{
+		Task<string> SetUpWebHook(TrelloWebhook webHook, string apiKey, string token);
+	}
+}
