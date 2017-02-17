ec348b80ad712a5e1d99bc0e8b7e1ce1a9bd070b
andmos <andreas.mosti@gmail.com>
2017-02-17 21:43:04 +0100

    interface doc

--- ReadingList/Trello/IWebHookCaller.cs
+++ ReadingList/Trello/IWebHookCaller.cs
@@ -3,8 +3,18 @@ using System.Threading.Tasks;
 
 namespace ReadingList
 {
+	/// <summary>
+	/// Web hook caller.
+	/// </summary>
 	public interface IWebHookCaller
 	{
+		/// <summary>
+		/// Sets up web hook, fire and forget style
+		/// </summary>
+		/// <returns>The up web hook.</returns>
+		/// <param name="webHook">Web hook.</param>
+		/// <param name="apiKey">API key.</param>
+		/// <param name="token">Token.</param>
 		Task SetUpWebHook(TrelloWebhook webHook, string apiKey, string token);
 	}
 }
