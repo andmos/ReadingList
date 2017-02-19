0ae2345bc506c2330527aef550d15242bc3905ff
andmos <andreas.mosti@gmail.com>
2017-02-19 11:46:16 +0100

    interface documentation

--- ReadingList/Trello/ITrelloAuthModel.cs
+++ ReadingList/Trello/ITrelloAuthModel.cs
@@ -1,9 +1,21 @@
 ﻿using System;
 namespace ReadingList
 {
+	/// <summary>
+	/// Trello auth model.
+	/// </summary>
 	public interface ITrelloAuthModel
 	{
+		/// <summary>
+		/// Gets the trello API Key.
+		/// </summary>
+		/// <value>The trello API Key.</value>
 		string TrelloAPIKey { get; }
+
+		/// <summary>
+		/// Gets the trello user token.
+		/// </summary>
+		/// <value>The trello user token.</value>
 		string TrelloUserToken { get; }
 	}
 }
