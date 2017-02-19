30525bb8491d70f934eb7cf063d4d389a99e913f
andmos <andreas.mosti@gmail.com>
2017-02-19 12:26:50 +0100

    more interface documentation

--- ReadingList/Logging/ILog.cs
+++ ReadingList/Logging/ILog.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 namespace ReadingList
 {
+	/// <summary>
+	/// Logger interface
+	/// </summary>
 	public interface ILog
 	{
+		/// <summary>
+		///  Log the specified message as Info
+		/// </summary>
+		/// <param name="message">Message.</param>
 		void Info(string message);
 
+		/// <summary>
+		/// Log the specified message as Debug
+		/// </summary>
+		/// <param name="message">Message.</param>
 		void Debug(string message);
 
+		/// <summary>
+		/// Log the specified error message and exception.
+		/// </summary>
+		/// <param name="message">Message.</param>
+		/// <param name="exception">Exception.</param>
 		void Error(string message, Exception exception = null);
 	}
 }
--- ReadingList/Logging/ILogFactory.cs
+++ ReadingList/Logging/ILogFactory.cs
@@ -1,8 +1,16 @@
 ﻿using System;
 namespace ReadingList
 {
+	/// <summary>
+	/// Log factory.
+	/// </summary>
 	public interface ILogFactory
 	{
+		/// <summary>
+		/// Gets the logger.
+		/// </summary>
+		/// <returns>The logger.</returns>
+		/// <param name="type">Type.</param>
 		ILog GetLogger(Type type);
 	}
 }
