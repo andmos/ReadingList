a6725427a80b160c92314c137a4ae309da77466d
andmos <andreas.mosti@gmail.com>
2017-07-18 11:33:41 +0200

    removed output and added comment

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,7 @@
 binary=/ReadingList/bin/Release/ReadingList.exe
 configFile=/ReadingList/bin/Release/ReadingList.exe.config
 
-echo "$APIKey"
-
+# If a modified config file is in place at build time, we leave it. 
 sed -i -e "s/\<APIKey\>/$APIKey/g" $configFile
 sed -i -e "s/\<UserToken\>/$UserToken/g" $configFile
 sed -i -e "s/\<Url\>/$HostURL/g" $configFile 
