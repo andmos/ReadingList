ff7880afed0aa9bf686ce3c090aa0bc34232acec
andmos <andreas.mosti@gmail.com>
2017-09-09 19:23:57 +0200

    Better view of available API methods

---
+++ ReadingList/Models/IndexModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Nancy.Routing;
+namespace ReadingList
+{
+	internal class IndexModel
+	{
+		public IEnumerable<RouteDescription> Routes { get; set; }
+	}
+
+	internal class RouteMetadata
+	{
+		public string Path { get; set; }
+		public string Method { get; set; }
+	}
+}
