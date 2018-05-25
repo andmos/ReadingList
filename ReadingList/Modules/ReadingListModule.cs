7f96da0a0a8a3d205ed7a63652fc59dcbaca46ca
andmos <andreas.mosti@gmail.com>
2018-05-25 20:00:27 +0200

    cleaned up modules for #17

--- ReadingList/Modules/ReadingListModule.cs
+++ ReadingList/Modules/ReadingListModule.cs
@@ -6,45 +6,28 @@ using System.Collections.Generic;
 
 namespace ReadingList
 {
-	public class ReadingListModule : NancyModule 
+	public class ReadingListModule : NancyModule
 	{
 		private readonly IReadingListService m_readingListService;
 		private readonly ITrelloWebHookSources m_webHookSource;
-		private readonly IRouteCacheProvider m_routeCacheProvider;
 		private readonly IReadingBoardService m_readingBoardService;
 		private readonly ILog m_logger;
 
-		public ReadingListModule(ITrelloAuthorizationWrapper trelloAuthService, IReadingListService readingListService, IReadingBoardService readingBoardService, ITrelloWebHookSources webHookSource, ILogFactory logger, IRouteCacheProvider routeCacheProvider) : base("/api/")
+		public ReadingListModule(ITrelloAuthorizationWrapper trelloAuthService, IReadingListService readingListService, IReadingBoardService readingBoardService, ITrelloWebHookSources webHookSource, ILogFactory logger) : base("/api/")
 		{
 			this.EnableCors();
 			m_readingListService = readingListService;
 			m_readingBoardService = readingBoardService;
 			m_webHookSource = webHookSource;
-			m_routeCacheProvider = routeCacheProvider; 
-			m_logger = logger.GetLogger(GetType()); 
+			m_logger = logger.GetLogger(GetType());
 			StaticConfiguration.EnableHeadRouting = true;
 			StaticConfiguration.DisableErrorTraces = false;
-
-			Get["/"] = parameters =>
-			{
-				var responseObject = new IndexModel();
-				var cache = m_routeCacheProvider.GetCache();
-
-				responseObject.Routes = cache.Values.SelectMany(t => t.Select(t1 => t1.Item2));
-
-				return Response.AsJson(responseObject.Routes.Select(p => new KeyValuePair<string, string>(p.Path, p.Method)));
-			};
-
-			Get["/ping"] = parameters =>
-			{
-				return "pong";
-			};
 
 			Get["/readingList"] = parameters =>
 			{
 				string requestLabel = Request.Query["label"];
 				var readingList = m_readingListService.GetReadingList(TrelloBoardConstans.CurrentlyReading, requestLabel);
-				return Response.AsJson(readingList); 
+				return Response.AsJson(readingList);
 			};
 
 			Get["/backlogList"] = parameters =>
@@ -72,13 +55,13 @@ namespace ReadingList
 				string author = Request.Query["author"];
 				string bookTitle = Request.Query["title"];
 				string bookLabel = Request.Query["label"];
-				Response response; 
+				Response response;
 
 				if (string.IsNullOrWhiteSpace(author) || string.IsNullOrWhiteSpace(bookTitle) || string.IsNullOrWhiteSpace(bookLabel))
 				{
 					response = Response.AsJson("author, title and label is required.");
 					response.StatusCode = HttpStatusCode.UnprocessableEntity;
-					return response; 
+					return response;
 				}
 				try
 				{
@@ -88,7 +71,7 @@ namespace ReadingList
 						response.StatusCode = HttpStatusCode.Created;
 
 					}
-					else 
+					else
 					{
 						response = Response.AsJson("Something went wrong when adding book to list");
 						response.StatusCode = HttpStatusCode.InternalServerError;
@@ -96,7 +79,7 @@ namespace ReadingList
 
 					return response;
 				}
-				catch(Exception ex) 
+				catch(Exception ex)
 				{
 					response = Response.AsJson(ex.ToString());
 					response.StatusCode = HttpStatusCode.InternalServerError;
@@ -118,7 +101,7 @@ namespace ReadingList
 					m_readingListService.UpdateDoneListFromReadingList(bookTitle);
 					response = Response.AsJson("updated");
 					response.StatusCode = HttpStatusCode.OK;
-					return response; 
+					return response;
 				}
 				catch (Exception ex)
 				{
@@ -136,30 +119,30 @@ namespace ReadingList
 				return respons;
 			};
 
-			Post["/callBack"] = parameters => 
+			Post["/callBack"] = parameters =>
 			{
-				Response respons; 
+				Response respons;
 
-				if (m_webHookSource.ValidWebhookSources().ToList().Any(source => source.Equals(Request.UserHostAddress))) 
+				if (m_webHookSource.ValidWebhookSources().ToList().Any(source => source.Equals(Request.UserHostAddress)))
 				{
-					m_logger.Info($"Got Callback from valid source: {Request.UserHostAddress}"); 
+					m_logger.Info($"Got Callback from valid source: {Request.UserHostAddress}");
 				}
 
 				// TODO: This is a bit leaky..
-				if (m_readingListService is ReadingListCache) 
+				if (m_readingListService is ReadingListCache)
 				{
 					((ReadingListCache)m_readingListService).InvalidateCache();
-					m_logger.Info("Invalidating cache"); 
+					m_logger.Info("Invalidating cache");
 				}
 
 				respons = Response.AsJson("Callback recived");
 				respons.StatusCode = HttpStatusCode.OK;
 				return respons;
 			};
-				
-		
-		
+
+
+
 		}
-	
+
 	}
 }
---
+++ ReadingList/Modules/RouteModule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nancy;
+using Nancy.Routing;
+
+namespace ReadingList
+{
+	public class RouteModule : NancyModule
+	{
+		private readonly IRouteCacheProvider m_routeCacheProvider;
+	
+		public RouteModule(IRouteCacheProvider routeCacheProvider) : base("/api/")
+		{
+			m_routeCacheProvider = routeCacheProvider; 
+            this.EnableCors();
+			Get["/"] = parameters =>
+			{
+				var responseObject = new IndexModel();
+				var cache = m_routeCacheProvider.GetCache();
+
+				responseObject.Routes = cache.Values.SelectMany(t => t.Select(t1 => t1.Item2));
+
+				return Response.AsJson(responseObject.Routes.Select(p => new KeyValuePair<string, string>(p.Path, p.Method)));
+			};
+
+			Get["/ping"] = parameters =>
+			{
+				return "pong";
+			};
+		}
+	}
+}
