357bdb2b57a3eda733f034893c7951869705d796
andmos <andreas.mosti@gmail.com>
2018-11-03 21:47:46 +0100

    renamed IBookParser to IBookFactory

---
+++ ReadingList/Helpers/IBookFactory.cs
@@ -0,0 +1,14 @@
+﻿namespace ReadingList
+{
+	public interface IBookFactory
+	{
+		/// <summary>
+		/// Create the Book object from specified bookString and listLabel.
+		/// 
+		/// </summary>
+		/// <returns>The Book Object</returns>
+		/// <param name="bookString">Book string. format: [BookTitle - Author1, Author2]</param>
+		/// <param name="listLabel">List string.</param>
+		Book Create(string bookString, string listLabel); 
+	}
+}
