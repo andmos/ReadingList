eda9afe9ac021f34f3bcb3a58162a6b90c5072c6
andmos <andreas.mosti@gmail.com>
2019-03-06 17:23:25 +0100

    segregated cache as own component for better devalidation

---
+++ ReadingList/Helpers/ReadingListCache.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+
+namespace ReadingList.Helpers
+{
+	public class ReadingListCache : IReadingListCache
+    {
+		private readonly ConcurrentDictionary<KeyValuePair<string, string>, IEnumerable<Book>> m_cache;
+
+        public ReadingListCache()
+        {
+			m_cache = new ConcurrentDictionary<KeyValuePair<string, string>, IEnumerable<Book>>();
+		}
+
+		public bool TryGetValue(KeyValuePair<string, string> listLabelPair, out IEnumerable<Book> listOut)
+		{
+			return m_cache.TryGetValue(listLabelPair, out listOut);
+		}
+
+		public bool TryAdd(KeyValuePair<string, string> listLabelPair, IEnumerable<Book> listOut)
+		{
+			return m_cache.TryAdd(listLabelPair, listOut);
+		}
+
+		public void InvalidateCache()
+        {
+			m_cache.Clear();
+        }
+	}
+}
