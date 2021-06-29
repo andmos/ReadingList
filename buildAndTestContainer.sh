0b70173dd3f907ff42d3d49bb326252773ae055b
Andreas Mosti <andreas.mosti@gmail.com>
2021-06-29 11:14:30 +0200

    using trap in test-script to do propper cleanup

--- buildAndTestContainer.sh
+++ buildAndTestContainer.sh
@@ -2,10 +2,27 @@
 
 set -e
 
+export GIT_REV=$(git rev-parse --short HEAD)
+
+cleanup(){
+    echo "cleaning up containers"
+    docker stop readinglist
+    docker rm readinglist
+    echo "cleanup completed"
+}
+
+trap "cleanup" ERR
+
+echo "Building readinglist image with git rev $GIT_REV"
 docker build -t andmos/readinglist:$(git rev-parse --short HEAD) .
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/tests gcr.io/gcp-runtimes/container-structure-test:v1.8.0 test --image andmos/readinglist:$(git rev-parse --short HEAD) --config /tests/imageTests/readinglist_container_tests_config.yaml
-docker run --name readinglist -dt -p 1337:1337 -e TrelloAuthSettings__TrelloAPIKey=$TrelloAPIKey -e TrelloAuthSettings__TrelloUserToken=$TrelloUserToken andmos/readinglist:$(git rev-parse --short HEAD)
+
+echo "Running container structure tests on readinglist image"
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/tests gcr.io/gcp-runtimes/container-structure-test:v1.8.0 test --image andmos/readinglist:$GIT_REV --config /tests/imageTests/readinglist_container_tests_config.yaml
+
+echo "Starting readinglist image and running bats tests"
+docker run --name readinglist -dt -p 1337:1337 -e TrelloAuthSettings__TrelloAPIKey=$TrelloAPIKey -e TrelloAuthSettings__TrelloUserToken=$TrelloUserToken andmos/readinglist:$GIT_REV
 sleep 5
 docker run --link readinglist:readinglist --rm -e TrelloAPIKey=$TrelloAPIKey -e TrelloUserToken=$TrelloUserToken -v $(pwd):/app graze/bats /app/batsTests
-docker stop readinglist
-docker rm readinglist
+
+
+cleanup
