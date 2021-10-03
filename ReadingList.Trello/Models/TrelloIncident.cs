a77a28f244686de4f1591b6b9abbaaf32b2eeda4
andmos <andreas.mosti@gmail.com>
2021-10-03 13:02:06 +0200

    Trello Status Health Service
    Began work on Trello Status Service to see if existing incidents are under way.

---
+++ ReadingList.Trello/Models/TrelloIncident.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ReadingList.Trello.Models
+{
+    public class TrelloIncident
+    {
+        public string Title { get; set; }
+        public DateTimeOffset Published { get; set; }
+        public DateTimeOffset Updated { get; set; }
+        public string Content { get; set; }
+        public bool Resolved => Content.ToLower().Contains("resolved");
+    }
+}
---
+++ ReadingList.Trello/Services/ITrelloStatusService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using ReadingList.Trello.Models;
+
+namespace ReadingList.Trello.Services
+{
+    public interface ITrelloStatusService
+    {
+        IEnumerable<TrelloIncident> GetStatusIncidents();   
+    }
+}
