320bd30c9b4db010b900acc763fc86343b5644f1
Andreas Mosti <andreas.mosti@gmail.com>
2022-03-29 13:09:51 +0200

    Moved reading list constants to logic project (#104)
    The name of the lists has nothing to do with Trello percé, so these are moved to the Logic-project.
    
    Co-authored-by: Andreas Mosti <1283556+andmos@users.noreply.github.com>

--- ReadingList.Trello/Models/TrelloBoardConstans.cs
+++ ReadingList.Trello/Models/TrelloBoardConstans.cs
@@ -2,14 +2,6 @@ namespace ReadingList.Trello.Models
 {
     public static class TrelloBoardConstans
     {
-        public static string Backlog => "Backlog";
-        public static string CurrentlyReading => "Reading";
-        public static string UpNext => "To Read";
-        public static string DoneReading => "Done";
         public static string BoardId => "hWsZ9uhl";
-        public static string UnspecifiedLabel => "Unspecified";
-        public static string ReadingListId => "51c1bff352ec1db00f003e96";
-        public static string BoardWebhookId => "510d1b2bca2dfcea6c0016e3";
-
     }
 }
