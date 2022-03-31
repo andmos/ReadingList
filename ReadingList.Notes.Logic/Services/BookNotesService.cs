fd35a8cdfbfa341640aa6eda755c765a505a8a83
Andreas Mosti <1283556+andmos@users.noreply.github.com>
2022-03-31 08:02:04 +0200

    Removed possible multiple enumerations

--- ReadingList.Notes.Logic/Services/BookNotesService.cs
+++ ReadingList.Notes.Logic/Services/BookNotesService.cs
@@ -18,7 +18,7 @@ namespace Readinglist.Notes.Logic.Services
 
         public async Task<BookNote> GetRandomBookNote()
         {
-            var records = await _bookRecordRepository.GetAllBookRecords();
+            var records = (await _bookRecordRepository.GetAllBookRecords()).ToList();
             var randomRecord = records.ElementAt(_randomizer.Next(0, records.Count()));
             var randomNoteFromBook = randomRecord.Notes.ElementAt(_randomizer.Next(0, randomRecord.Notes.Count()));
 
