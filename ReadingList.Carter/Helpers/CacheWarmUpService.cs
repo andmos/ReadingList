f24a35ad73979880310ea20565841170fcda1748
Andreas Mosti <andreas.mosti@gmail.com>
2022-12-12 13:28:34 +0100

    Adding some logging to cache warmup

--- ReadingList.Carter/Helpers/CacheWarmUpService.cs
+++ ReadingList.Carter/Helpers/CacheWarmUpService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Readinglist.Notes.Logic.Services;
+using ReadingList.Logging;
 using ReadingList.Logic.Models;
 using ReadingList.Logic.Services;
 
@@ -10,15 +11,20 @@ namespace ReadingList.Carter.Helpers
     {
         private readonly IReadingListService _readingListService;
         private readonly IBookNotesService _bookNotesService;
+        private readonly ILog _logger;
 
-        public CacheWarmUpService(IReadingListService readingListService, IBookNotesService bookNotesService)
+        public CacheWarmUpService(IReadingListService readingListService, 
+            IBookNotesService bookNotesService,
+            ILogFactory logFactory)
         {
             _readingListService = readingListService;
             _bookNotesService = bookNotesService;
+            _logger = logFactory.GetLogger(this.GetType());
         }
 
         public async Task WarmUpCaches()
         {
+            _logger.Info("Starting cache warmup");
             var cacheTasks = new List<Task> 
             {
                 _bookNotesService.GetAllBookNotes(),
