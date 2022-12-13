a5be73c41bf466444406ef0d6e8dac8cb1a93729
Andreas Mosti <andreas.mosti@gmail.com>
2022-12-13 19:23:06 +0100

    Linebreak constructor

---
+++ ReadingList.Carter/Helpers/CacheWarmUpService.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Readinglist.Notes.Logic.Services;
+using ReadingList.Logging;
+using ReadingList.Logic.Models;
+using ReadingList.Logic.Services;
+
+namespace ReadingList.Carter.Helpers
+{
+    public class CacheWarmUpService
+    {
+        private readonly IReadingListService _readingListService;
+        private readonly IBookNotesService _bookNotesService;
+        private readonly ILog _logger;
+
+        public CacheWarmUpService(
+            IReadingListService readingListService, 
+            IBookNotesService bookNotesService,
+            ILogFactory logFactory)
+        {
+            _readingListService = readingListService;
+            _bookNotesService = bookNotesService;
+            _logger = logFactory.GetLogger(GetType());
+        }
+
+        public async Task WarmUpCaches()
+        {
+            _logger.Info("Starting cache warmup");
+            var cacheTasks = new List<Task> 
+            {
+                _bookNotesService.GetAllBookNotes(),
+                _readingListService.GetReadingList(ReadingListConstants.Backlog),
+                _readingListService.GetReadingList(ReadingListConstants.CurrentlyReading),
+                _readingListService.GetReadingList(ReadingListConstants.DoneReading)
+            };
+            
+            await Task.WhenAll(cacheTasks);
+        }
+    }
+}
