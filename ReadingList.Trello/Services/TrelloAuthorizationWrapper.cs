299c2a3283a58960b36070c4de8301365de99cd9
Andreas Mosti <andreas.mosti@gmail.com>
2022-12-13 19:11:11 +0100

    Revert "Revert "Added CacheWarmUpService with Hangfire"" (#179)
    * Revert "Revert "Added CacheWarmUpService with Hangfire (#176)" (#178)"
    
    This reverts commit d4a2c6a582c72d74e4555b024bb55c8eec5ae9c2.
    
    * Making sure ReadingListService is authenticated before requests against Trello

--- ReadingList.Trello/Services/TrelloAuthorizationWrapper.cs
+++ ReadingList.Trello/Services/TrelloAuthorizationWrapper.cs
@@ -6,7 +6,6 @@ namespace ReadingList.Trello.Services
 {
     public class TrelloAuthorizationWrapper : ITrelloAuthorizationWrapper
     {
-
         private readonly ITrelloAuthModel _authModel;
 
         public TrelloAuthorizationWrapper(ITrelloAuthModel authModel)
@@ -15,16 +14,18 @@ namespace ReadingList.Trello.Services
             {
                 throw new ArgumentNullException();
             }
-
             _authModel = authModel;
+        }
 
+        public void Authenticate()
+        {
             TrelloAuthorization.Default.AppKey = _authModel.TrelloAPIKey;
             TrelloAuthorization.Default.UserToken = _authModel.TrelloUserToken;
         }
 
         public bool IsValidKeys(ITrelloAuthModel authModel)
         {
-            return (authModel.TrelloAPIKey.Equals(_authModel.TrelloAPIKey) && authModel.TrelloUserToken.Equals(_authModel.TrelloUserToken));
+            return authModel.TrelloAPIKey.Equals(_authModel.TrelloAPIKey) && authModel.TrelloUserToken.Equals(_authModel.TrelloUserToken);
         }
     }
 }
