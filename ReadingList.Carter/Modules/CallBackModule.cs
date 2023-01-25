017c81c860021dd6b5af950020c0aa03364205e9
Andreas Mosti <andreas.mosti@gmail.com>
2023-01-25 18:30:28 +0100

    Removed callback validation code
    Will be moved to nginx config

--- ReadingList.Carter/Modules/CallBackModule.cs
+++ ReadingList.Carter/Modules/CallBackModule.cs
@@ -1,7 +1,5 @@
-using System.Linq;
 using ReadingList.Logging;
 using ReadingList.Trello.Helpers;
-using ReadingList.Trello.Services;
 using Carter;
 using Carter.Response;
 using Microsoft.AspNetCore.Routing;
@@ -13,24 +11,21 @@ namespace ReadingList.Web.Modules
     public class CallbackModule : ICarterModule
     {
         private readonly IReadingListCache _readingListCache;
-        private readonly ITrelloWebHookSources _webHookSourceService;
         private readonly ILog _logger;
 
         private const string BaseUri = "/api";
 
         public CallbackModule(
             IReadingListCache readingListCache,
-            ILogFactory logger,
-            ITrelloWebHookSources webHookSource)
+            ILogFactory logger)
         {
             _logger = logger.GetLogger(GetType());
             _readingListCache = readingListCache;
-            _webHookSourceService = webHookSource;
         }
 
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapMethods($"{BaseUri}/callBack", new[] {"HEAD"}, async (HttpRequest req, HttpResponse res) =>
+            app.MapMethods($"{BaseUri}/callBack", new[] { "HEAD" }, async (HttpRequest req, HttpResponse res) =>
             {
                 res.StatusCode = 200;
                 await res.AsJson("Head received");
@@ -39,18 +34,12 @@ namespace ReadingList.Web.Modules
             app.MapPost($"{BaseUri}/callBack", async (HttpRequest req, HttpResponse res) =>
             {
                 var callerIp = req.HttpContext.Connection.RemoteIpAddress;
-                if (_webHookSourceService.IsValidWebHookSource(callerIp))
-                {
-                    _logger.Info($"Got Callback from valid source: {callerIp}");
-                    _readingListCache.InvalidateCache();
-                    _logger.Info("Invalidating cache");
-                    res.StatusCode = 200;
-                    await res.AsJson("Callback received");
-                }
-                else
-                {
-                    _logger.Info($"Got Callback from non-valid source: {callerIp}");
-                }
+
+                _logger.Info($"Got Callback from source: {callerIp}");
+                _readingListCache.InvalidateCache();
+                _logger.Info("Invalidating cache");
+                res.StatusCode = 200;
+                await res.AsJson("Callback received");
             });
         }
     }
